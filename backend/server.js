c126a7e65f141c0ea1075d2ca39b2d4c1623b67e
Anjina Shahi <np03cs4a220179@heraldcollege.edu.np>
2026-02-10 22:03:57 +0545

    first commit

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
+require("dotenv").config();
+
+const app = express();
+connectDB();
+app.use(cors());
+app.use(express.json());
+app.use("/api/auth", require("./routes/authRoutes"));
+
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
